5782b615b2c34a041e43e38db417f7e3a5495800
Alexmirasan <camdsoa@gmail.com>
2023-12-05 17:51:08 -0300

    primeiro envio de projeto

--- desafiodio1.js
+++ desafiodio1.js
@@ -1,1 +1,46 @@
-console.log("vamos começar")
+let nome = "Alexandre"
+let xp =10000
+let nivel = ""
+
+switch(true){
+
+    case xp <= 1000: 
+        nivel = "ferro"
+        console.log("O heroi de nome " + nome + " está no nível " + nivel + ".")
+    break;
+    
+    case xp >= 1001 && xp <= 2000: 
+        nivel = "bronze"
+        console.log("O heroi de nome " + nome + " está no nível " + nivel + ".")
+    break;
+
+    case xp >= 2001 && xp <= 5000: 
+        nivel = "prata"
+        console.log("O heroi de nome " + nome + " está no nível " + nivel + ".")
+    break;
+    
+    case xp >= 5001 && xp <= 7000: 
+        nivel = "ouro"
+        console.log("O heroi de nome " + nome + " está no nível " + nivel + ".")
+    
+    case xp >= 7001 && xp <= 8000 :
+        nivel = "platina"
+        console.log("O heroi de nome " + nome + " está no nível " + nivel + ".")
+    break;
+
+    case xp >= 8001 && xp <= 9000 :  
+        nivel = "ascendente"
+        console.log("O heroi de nome " + nome + " está no nível " + nivel + ".")
+    break;
+    
+    case xp >= 9001 && xp >= 10000 :  
+        nivel = "imortal"
+        console.log("O heroi de nome " + nome + " está no nível " + nivel + ".")
+    break;
+    
+    case xp >= 10001: 
+        nivel = "radiante"
+        console.log("O heroi de nome " + nome + " está no nível " + nivel + ".")
+    
+    default: console.log("valor invalido!")
+}
